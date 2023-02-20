e00ae0162fd787f0a1889fa80149fa0452bb3e17
pacocerezo <pacoceval@gmail.com>
2023-02-20 11:45:55 +0100

    Disabled setting API KEY for deployment

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ Session(app)
 db = SQL("sqlite:///finance.db")
 
 # Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
+#if not os.environ.get("API_KEY"):
+#    raise RuntimeError("API_KEY not set")
 
 
 @app.after_request
